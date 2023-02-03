09f26eb904baf8ca06165847c66e0a3615280a3f
wenchijchang <heyyhey@hotmail.com>
2023-02-03 14:53:33 -0500

    created html and JS files

---
+++ worksheet.js
@@ -0,0 +1,11 @@
+"use strict";
+
+//Task1 dayOfWeek
+let dayOfWeek = "Monday"
+console.log(dayOfWeek);
+console.log("I can't wait for Friday!");
+
+//Task2 userInput
+let animalInput = prompt("What is your favorite animal?")
+let colorInput = prompt("What is your favorite color")
+console.log("I've never seen a colorInput + ' ' + animalInput!");
